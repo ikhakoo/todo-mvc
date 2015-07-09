6543fc6fb4314b6e05b4809dd1fd6ee6a6926d71
ikhakoo <ikhakoo@gmail.com>
2015-07-09 12:49:21 -0400

    update component to include accept changes, is completed and edit

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -5,18 +5,16 @@ export default Ember.Route.extend({
         return this.store.find('todo');
     },
     actions: {
-        createTodo: function(newTitle) {
-            // Create the new Todo model
-            var todo = this.store.createRecord('todo', {
-                title: newTitle,
-                isCompleted: false
-            });
-            
-            // Clear the "New Todo" text field
-            this.controllerFor('todos').set('newTitle', '');
- 
-            // Save the new model
-            todo.save();
+        // Additional lines truncated for brevity...
+        acceptChanges: function(todo) {
+            if (Ember.isEmpty(todo.get('title'))) {
+                this.send('deleteTodo', todo);
+            } else {
+                todo.save();
+            }
+        },
+        deleteTodo: function(todo) {
+            todo.deleteRecord();
         }
     }
 });
